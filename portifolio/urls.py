758704f880e0d3530e2a02f235c828eec304564a
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-09-13 15:49:22 -0300

    Initial django settings and portfolio frontend.

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path
+
+from . import views
+
+app_name = 'portifolio'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('portifolio.urls')),
+    path('', views.home, name='home'),
 ]
