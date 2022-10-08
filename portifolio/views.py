7390dbba4d5cca8e46d6bd2f4de385c6aa25bce2
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-10-08 10:55:55 -0300

    Added create edit and delete of socialmedia minicard.

--- portifolio/views.py
+++ portifolio/views.py
@@ -1,29 +1,32 @@
 from django.http import HttpResponse
+from django.http.response import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import PersonalDataForm
-from .models import About, BarProgress, Card, PersonalData, SocialMedia
+from .forms import MiniCardForm, PersonalDataForm
+from .models import About, BarProgress, Card, MiniCard, PersonalData
 
 
 # Create your views here.
 def home(request):
     template_name = 'portifolio/pages/home.html'
     personaldatas = PersonalData.objects.all()[:1]
-    socialmedias = SocialMedia.objects.all()
+    minicards = MiniCard.objects.all()
     about = About.objects.all()[:1]
     barprogress = BarProgress.objects.all()
     cards = Card.objects.all()
 
     form = PersonalDataForm
+    form_minicard = MiniCardForm
     context = {
         'personaldatas': personaldatas,
-        'socialmedias': socialmedias,
+        'minicards': minicards,
         'about': about,
         'barprogress': barprogress,
         'cards': cards,
 
         'form': form,
+        'form_minicard': form_minicard,
     }
     return render(request, template_name, context)
 
@@ -55,3 +58,46 @@ def editpersonaldata(request):
             id=id, name=name, profession=profession, title=title, whatsapp=whatsapp)
         personaldata.save()
         return redirect(reverse('portifolio:home'))
+
+
+def createsocialmedia(request):
+    form = MiniCardForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect(reverse('portifolio:home'))
+
+    context = {
+        'form': form,
+    }
+    return render(request, context)
+
+
+def editsocialmedia(request):
+    if request.method == 'POST':
+        id = request.POST.get('minicard_id')
+        name = request.POST.get('name')
+        icon = request.POST.get('icon')
+        link = request.POST.get('link')
+
+        socialmedia = MiniCard.objects.get(id=id)
+
+        socialmedia = MiniCard(
+            id=id, name=name, icon=icon, link=link)
+        socialmedia.save()
+        return redirect(reverse('portifolio:home'))
+
+
+def deleteminicard(request, minicard_id):
+    if not request.POST:
+        raise Http404()
+
+    minicard = MiniCard.objects.get(id=minicard_id)
+
+    if not minicard:
+        raise Http404()
+    minicard.delete()
+
+    return redirect(reverse('portifolio:home'))
+
+    return HttpResponse('Mini Card Apagado com sucesso!')
