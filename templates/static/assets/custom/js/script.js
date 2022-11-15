59155b9a5619514cc2433751994275cacac53bb6
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-11-15 18:02:01 -0300

    Modified css and added helps for the user.

--- templates/static/assets/custom/js/script.js
+++ templates/static/assets/custom/js/script.js
@@ -26,18 +26,20 @@ themeToggler.onclick = () =>{
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
   
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
-        }
       }
     }
   }
+}
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
